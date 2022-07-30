d5a4ac424a78486980a189e8cd9a9cf2d4eb041f
LIL-KOTYO <52920453+LILKOTYO@users.noreply.github.com>
2022-07-30 18:11:01 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+// Debug Settings
+// --------------------------------------
+#define RYAO_ARROW_DEBUG
+
+// Include Header Files
+// --------------------------------------
+#include <iostream>
+#include <Arrow.h>
+#include <Eigen/Core>
+
+int main(int argc, char** argv)
+{
+    Ryao::Logger::Init();
+    Ryao::Arrow arrow(Eigen::Vector3d({1.0, 2.0, 3.0}), Eigen::Vector3d({2.0, 6.0, 4.0}));
+    return 0;
+}
