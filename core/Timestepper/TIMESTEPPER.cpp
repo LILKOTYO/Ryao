bb0e362b0a118495a8e6e0b790d357a6340db737
LILKOTYO <lilkotyo@gmail.com>
2022-12-01 17:06:39 +0800

    update timestepper.cpp

--- core/Timestepper/TIMESTEPPER.cpp
+++ core/Timestepper/TIMESTEPPER.cpp
@@ -2,6 +2,7 @@
 #include <Timer.h>
 #include <TET_Mesh_Faster.h>
 #include <float.h>
+#include <Logger.h>
 
 using namespace std;
 
@@ -98,7 +99,107 @@ void TIMESTEPPER::buildConstraintMatrix() {
         const VECTOR3 normal = shape->localNormalToWorld(localNormal).normalized();
 
         // build the filter matrix
-        
+        const MATRIX3 Sblock = MATRIX3::Identity() - normal * normal.transpose();
+        const int vertexID = constraint.vertexID;
+        const int index = vertexID * 3;
+        for (int j = 0; j < 3; j++)
+            for (int i = 0; i < 3; i++)
+                _S.coeffRef(index + i, index + j) = Sblock(i, j);
+    }
+
+    // apply the kinematic constraints LAST. These override any prior plane constraints
+    for (unsigned int x = 0; x < _kinematicConstraints.size(); x++) {
+        const KINEMATIC_CONSTRAINT& constraint = _kinematicConstraints[x];
+
+        // set the filter matrix entries
+        const int index = 3 * constraint.vertexID;
+        for (int j = 0; j <  3; j++)
+            for (int i = 0; i < 3; i++) 
+                _S.coeffRef(index + i, index + j) = 0.0; 
+    }
+
+    // store the complement 
+    _IminusS = I - _S;
+}
+
+SPARSE_MATRIX TIMESTEPPER::buildMassMatrix() {
+    // build the triplets
+    typedef Eigen::Triplet<REAL> TRIPLET;
+    vector<TRIPLET> triplets;
+    const vector<REAL>&  volumes = _tetMesh.restOneRingVolumes();
+
+    // set diagonal the one-ring volumes
+    for (int x = 0; x < _tetMesh.totalVertices(); x++) {
+        const REAL entry = volumes[x];
+        for (int y = 0; y < 3; y++) {
+            TRIPLET triplet(x * 3 + y, x * 3 + y, entry);
+            triplets.push_back(triplet);
+        }
+    }
+
+    SPARSE_MATRIX A(_DOFs,_DOFs);
+    A.setFromTriplets(triplets.begin(), triplets.end());
+
+    return A;
+}
+
+SPARSE_MATRIX TIMESTEPPER::buildRayleighDampingMatrix() {
+    // back up current state 
+    _temp = _tetMesh.getDisplacement();
+
+    // set to zero displacement
+    VECTOR zero(_DOFs);
+    zero.setZero();
+    _tetMesh.setDisplacement(zero);
+
+    // get stiffness matrix at that state
+    _tetMesh.computeFs();
+    _tetMesh.computeSVDs();
+    SPARSE_MATRIX K = _tetMesh.computeHyperelasticClampedHessian(_hyperelastic);
+
+    // restore state
+    _tetMesh.setDisplacement(_temp);
+
+    // build out the Rayleigh damping
+    SPARSE_MATRIX C = _rayleighAlpha * _M + _rayleighBeta * K;
+    return C;
+}
+
+static void printEntry(const VECTOR& v, const int i, const string& varname) {
+    VECTOR3 v3;
+    v3[0] = v[3 * i];
+    v3[1] = v[3 * i + 1];
+    v3[2] = v[3 * i + 2];
+    RYAO_INFO("{}: {}", varname.c_str(), v3.transpose());
+}
+
+bool TIMESTEPPER::findSeparatingSurfaceConstraints(const VECTOR& unfiltered) {
+    bool changed = false;
+
+    for (unsigned int x = 0; x < _planeConstraints.size(); x++) {
+        PLANE_CONSTRAINT& constraint = _planeConstraints[x];
+
+        // is the vertex still inside the object? In that case, keep the constraint.
+        const KINEMATIC_SHAPE* shape = constraint.shape;
+        const int vertexID = constraint.vertexID;
+        const REAL signedDistance = shape->signedDistance(_tetMesh.vertices()[vertexID]);
+
+        bool debug = false;
+        if (debug) {
+            RYAO_DEBUG("Constraint for vertex: {}", constraint.vertexID);
+            RYAO_DEBUG("Signed distance: {}", signedDistance);
+        }
+
+        // if the distance is outside and large, move on
+        if (signedDistance > 1e-6) {
+            constraint.isSeparating = true;
+            changed = true;
+            if (debug) 
+                RYAO_DEBUG("CONSTRAINT IS SEPARATING");
+            continue;
+        }
+
+
     }
 }
 
