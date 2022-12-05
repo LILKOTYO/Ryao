076ef692b29ff37ec516990261f2760853b02377
LILKOTYO <lilkotyo@gmail.com>
2022-12-05 11:28:33 +0800

    update backward_euler_velocity.h

---
+++ core/Timestepper/Backward_Euler_Velocity.h
@@ -0,0 +1,41 @@
+#ifndef BACKWARD_EULER_VELOCITY_H
+#define BACKWARD_EULER_VELOCITY_H
+
+#include <TET_Mesh.h>
+#include <KINEMATIC_SHAPE.h>
+#include <CONSTRAINTS.h>
+#include <HYPERELASTIC.h>
+#include <TIMESTEPPER.h>
+
+namespace Ryao {
+namespace TIMESTEPPER {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// This is an implementation of the Baraff-Witkin-style velocity-level solver from
+// "Large Steps in Cloth Simulation", SIGGRAPH 1998
+//
+// [TJM15] refers to "Smoothed Aggregation Multigrid for Cloth Simulation", SIGGRAPH Asia 2015
+////////////////////////////////////////////////////////////////////////////////////////////////////
+class Backward_Euler_Velocity : public TIMESTEPPER {
+public:
+    Backward_Euler_Velocity(TET_Mesh& tetMesh, VOLUME::HYPERELASTIC& hyperelastic);
+
+private:
+    // update the displacement targets the Baraff-Witkin-style constraints
+    // are trying to hit. Assumes that buildConstraintMatrix() has already been called
+    virtual void updateConstraintTargets() override;
+
+    // Baraff-Witkin solves for change in velocity 
+    VECTOR _vDelta;
+
+    // current simulation time 
+    REAL _time;
+
+    // current simulation step
+    int _curretTimestep;
+};
+
+} // Ryao   
+} // TIMESTEPPER
+
+#endif
