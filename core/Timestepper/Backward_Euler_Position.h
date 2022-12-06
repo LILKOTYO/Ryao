481878182e0485848bbc4cfd3f139632956e2c8e
LILKOTYO <lilkotyo@gmail.com>
2022-12-06 16:54:06 +0800

    finished backward_euler_position.h

--- core/Timestepper/Backward_Euler_Position.h
+++ core/Timestepper/Backward_Euler_Position.h
@@ -17,6 +17,39 @@ namespace TIMESTEPPER {
 class Backward_Euler_Position : public TIMESTEPPER {
 public:
     Backward_Euler_Position(TET_Mesh& tetMesh, VOLUME::HYPERELASTIC& hyperelastic);
+    Backward_Euler_Position(TET_Mesh& tetMesh, VOLUME::HYPERELASTIC& hyperelastic, VOLUME::Damping& damping);
+    virtual ~Backward_Euler_Position();
+
+    const REAL rayleighAlpha() const    { return _rayleighAlpha; };
+    const REAL rayleighBeta() const     { return _rayleighBeta; };
+
+    const VECTOR velocityOld() const    { return _velocityOld; };
+    VECTOR velocityOld()                { return _velocityOld; };
+
+    // take a timestep
+    virtual bool solve(const bool verbose) override;
+    bool solveEnergyDamped(const bool verbose);
+
+    // solves with self collisions, and collision forces are Rayleigh damped
+    bool solveRayleighDamped(const bool verbose);
+
+private:
+    // shared initialization across constructors
+    void initialize();
+
+    // update the displacement targets the Baraff-Witkin-style constraints
+    // are trying to hit. Assumes that buildConstraintMatrix() has been called.
+    virtual void updateConstraintTargets() override;
+    
+    // current simulation time
+    REAL _time;
+
+    // current simulation step
+    int _currentTimestep;
+
+    // variables to solve for 
+    VECTOR _acceleration;
+    VECTOR _velocityOld;
 };
 
 }
